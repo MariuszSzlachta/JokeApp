1061fc1eb55d41c4cd0a68b622ccf991c1cd0007
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-28 17:33:55 +0200

    Create app

---
+++ script.js
@@ -0,0 +1,23 @@
+var button = document.querySelector('.get-joke');
+var output = document.querySelector('.joke');
+
+var url = 'http://api.icndb.com/jokes/random';
+
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+function getJoke(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response)
+    output.innerHTML = response.value.joke;
+  });
+
+  xhr.send();
+}
+
+window.addEventListener('load', function(){
+  getJoke();
+})
